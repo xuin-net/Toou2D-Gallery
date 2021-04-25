127ec2f3a5bba8a0e46372c71ae067cbb7da9616
xuinvip <xuin8640@gmail.com>
2021-04-25 21:59:40 +0800

    fix crash

--- cpp/restartapp.cpp
+++ cpp/restartapp.cpp
@@ -13,8 +13,8 @@ QObject* ReStartApp::initSignal(){
         // delete last object
         this->firstrootobj->deleteLater();
     }
-    //this->engine->load(QUrl(QStringLiteral("qrc:/com.aiyunji.lpm.app/main.qml")));
-    this->engine->load(QUrl(QStringLiteral("../../../../Toou2D-Gallery/main.qml")));
+    this->engine->load(QUrl(QStringLiteral("qrc:/main.qml")));
+//    this->engine->load(QUrl(QStringLiteral("$$PWD/../../../../Toou2D-Gallery/main.qml")));
     this->firstrootobj = this->engine->rootObjects().last();
     QObject::connect(this->firstrootobj,SIGNAL(reStartApp()),this,SLOT(appReload()));
     return this->firstrootobj;
